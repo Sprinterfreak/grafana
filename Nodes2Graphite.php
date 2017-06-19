dceea8ceebb5b34058d7921a5e34e5f50b2f7167
Sprinterfreak <janjonas@gmail.com>
2017-06-19 18:41:36 +0200

    Improved stability and memory usage

--- Nodes2Graphite.php
+++ Nodes2Graphite.php
@@ -36,38 +36,60 @@ class Nodes2Graphite
     {
         $nodeJson = file_get_contents($nodefile);
         $object = json_decode($nodeJson);
-        $nodes = $object->nodes;
+        $nodes = &$object->nodes;
         $online = 0;
         $clients = 0;
-	$firmwares = Array();
+		$firmwares = Array();
         foreach ($nodes as $node) {
-            $node_id = $node->nodeinfo->node_id;
-            $hostname = $this->sanitize($node->nodeinfo->hostname);
-            if ($node->flags->online === true) {
+            $node_id = $this->saveProp($node,'nodeinfo.node_id');
+            $hostname = $this->saveProp($node,'nodeinfo.hostname');
+            if ($this->saveProp($node, 'flags.online', false) === true) {
                 $nodeNamespace = $namespace . '.nodes.' . $node_id . '.' . $hostname . '.';
                 $online++;
-                $clients += $node->statistics->clients;
-		@$firmwares[$this->sanitize($node->nodeinfo->software->firmware->base)] += 1;
+                $clients += $this->saveProp($node, 'statistics.clients');
+				@$firmwares[$this->saveProp($node, 'nodeinfo.software.firmware.base', 'unknown')] += 1;
                 $this->sendToGraphite($nodeNamespace . 'flags.online', 1);
                 $this->sendToGraphite($nodeNamespace . 'hostname.' . $hostname, true);
-                $this->sendToGraphite($nodeNamespace . 'model.' . @$this->sanitize($node->nodeinfo->hardware->model), true);
-                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.tx', @$node->statistics->traffic->tx->bytes);
-                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.rx', @$node->statistics->traffic->rx->bytes);
-                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.mgmt_tx', @$node->statistics->traffic->mgmt_tx->bytes);
-                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.mgmt_rx', @$node->statistics->traffic->mgmt_rx->bytes);
-                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.forward', @$node->statistics->traffic->forward->bytes);
-                $this->sendToGraphite($nodeNamespace . 'statistics.loadavg', @$node->statistics->loadavg);
-                $this->sendToGraphite($nodeNamespace . 'statistics.memory_usage', @$node->statistics->memory_usage);
-                $this->sendToGraphite($nodeNamespace . 'statistics.uptime', @$node->statistics->uptime);
-                $this->sendToGraphite($nodeNamespace . 'statistics.clients', @$node->statistics->clients);
+                $this->sendToGraphite($nodeNamespace . 'model.' . $this->saveProp($node, 'nodeinfo.hardware.model', 'unknown'), true);
+                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.tx', $this->saveProp($node, 'statistics.traffic.tx.bytes'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.rx', $this->saveProp($node, 'statistics.traffic.rx.bytes'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.mgmt_tx', $this->saveProp($node, 'statistics.traffic.mgmt_tx.bytes'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.mgmt_rx', $this->saveProp($node, 'statistics.traffic.mgmt_rx.bytes'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.traffic.forward', $this->saveProp($node, 'statistics.traffic.forward.bytes'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.loadavg', $this->saveProp($node, 'statistics.loadavg'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.memory_usage', $this->saveProp($node, 'statistics.memory_usage'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.uptime', $this->saveProp($node, 'statistics.uptime'));
+                $this->sendToGraphite($nodeNamespace . 'statistics.clients', $this->saveProp($node, 'statistics.clients'));
             }
         }
         $this->sendToGraphite($namespace . '.mesh.online', $online);
         $this->sendToGraphite($namespace . '.mesh.clients', $clients);
-	foreach($firmwares as $ver => $ct) {
-		$this->sendToGraphite($namespace . '.firmware.' . $ver, $ct);
-	}
+		foreach($firmwares as $ver => $ct) {
+			$this->sendToGraphite($namespace . '.firmware.' . $ver, $ct);
+		}
     }
+	/**
+	 * Check $node structure integrity
+	 * @param object $node
+	 * @param string dot seperated prop path
+	 * @param string Fallback value
+	 */
+	private function saveProp($node, $propPath, $fallback = '')
+	{
+		$list = explode('.', $propPath);
+		foreach($list as $item) {
+			if(property_exists($node, $item)) {
+				$node = $node->$item;
+			} else {
+				return $fallback;
+			}
+		}
+		if(is_string($node))
+			return $this->sanitize($node);
+		elseif(is_numeric($node) || is_bool($node))
+			return $node;
+		return $fallback;
+	}
     /**
      * @param string $string UTF-8 input
      * @return string Allowed chars
